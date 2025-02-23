afe5497404926cd8690483f604cea1d34363c0f1
Hasini <hasinistudies@gmail.com>
2025-02-23 11:09:09 +0530

    first commit

---
+++ components/CustomButton.jsx
@@ -0,0 +1,57 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { theme } from "../constants/theme";
+import { hp } from "../helpers/common";
+import Loading from "./Loading";
+
+const CustomButton = ({
+  buttonStyle,
+  textStyle,
+  title = "",
+  onPress = () => {},
+  loading = false,
+  hasShowdow = true,
+}) => {
+  const shadowStyle = {
+    shadowColor: theme.colors.dark,
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 4,
+  };
+
+  if (loading) {
+    return (
+      <View style={[styles.button, buttonStyle, { backgroundColor: "white" }]}>
+        <Loading />
+      </View>
+    );
+  }
+
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[styles.button, buttonStyle, hasShowdow && shadowStyle]}
+    >
+      <Text style={[styles.text, textStyle]}>{title}</Text>
+    </Pressable>
+  );
+};
+
+export default CustomButton;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: theme.colors.primary,
+    height: hp(6.6),
+    justifyContent: "center",
+    alignItems: "center",
+    borderCurve: "continuous",
+    borderRadius: theme.radius.xl,
+  },
+  text: {
+    fontSize: hp(2.5),
+    color: "white",
+    fontWeight: theme.fonts.bold,
+  },
+});
